2d153e5e70666fe87ef24989cab7430e16cb829d
N.Sankov <ns@artskills-studio.ru>
2016-04-07 17:51:47 +0300

    fix SHA386 hash

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <?php
 file_put_contents('composer-setup.php', file_get_contents('https://getcomposer.org/installer'));
-if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') {
+if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') {
 	echo 'Installer verified';
 } else {
 	echo 'Installer corrupt';
